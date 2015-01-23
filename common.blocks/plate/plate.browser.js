ef04ea5d6753cdc9d534235c619975cd2764e38b
abukin <abukin@dw.local>
2015-01-23 16:19:48 +0400

    plate

---
+++ common.blocks/plate/plate.browser.js
@@ -0,0 +1,13 @@
+/**
+ * @module plate
+ */
+
+modules.define('plate', ['i-bem__dom'], function(provide, BEMDOM) {
+
+    /**
+     * @exports
+     * @class plate
+     * @bem
+     */
+    provide(BEMDOM.decl(this.name));
+});
