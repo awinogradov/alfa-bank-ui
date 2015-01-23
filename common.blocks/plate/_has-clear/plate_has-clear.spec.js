9c6de2dd410e994e5e9f86f3d4bdc52a29fde441
abukin <abukin@dw.local>
2015-01-23 18:18:42 +0400

    plate clear test

---
+++ common.blocks/plate/_has-clear/plate_has-clear.spec.js
@@ -0,0 +1,32 @@
+modules.define('spec', ['plate', 'i-bem__dom', 'jquery', 'BEMHTML'], function(provide, Plate, BEMDOM, $, BEMHTML) {
+
+describe('plate_has-clear', function() {
+    var plate;
+
+    beforeEach(function() {
+        plate = buildPlate();
+    });
+
+    afterEach(function() {
+        BEMDOM.destruct(plate.domElem);
+    });
+
+    it('should hide plate on click', function() {
+        plate.elem('clear').trigger('pointerclick');
+        plate.hasMod('hidden').should.be.true;
+    });
+});
+
+function buildPlate() {
+    return BEMDOM.init(
+            $(BEMHTML.apply({
+                block : 'plate',
+                mods : { theme : 'alfa', bkg : 'color', 'has-clear' : true }
+            })))
+        .appendTo('body')
+        .bem('plate');
+}
+
+provide();
+
+});
