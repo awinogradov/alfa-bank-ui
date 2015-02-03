30cdf44d6cf43f6da8cb34d19c4df1fd7b2137ab
Daniel Sabelnikov <dsabelnikov@gmail.com>
2015-02-03 18:26:53 +0300

    fix view_column popup position

--- common.blocks/popup/popup.browser.js
+++ common.blocks/popup/popup.browser.js
@@ -1,23 +1,5 @@
 modules.define('popup', ['objects'], function(provide, objects, Popup) {
-    var DEFAULT_DIRECTIONS = [
-        'bottom-left', 'bottom-center', 'bottom-right',
-        'top-left', 'top-center', 'top-right',
-        'right-top', 'right-center', 'right-bottom',
-        'left-top', 'left-center', 'left-bottom'
-    ];
-
     provide(Popup.decl(this.name, {
-        getDefaultParams : function() {
-            return objects.extend(
-                this.__base.apply(this, arguments),
-                {
-                    mainOffset : 0,
-                    secondaryOffset : 0,
-                    viewportOffset : 0,
-                    directions : DEFAULT_DIRECTIONS
-                });
-        },
-
         _calcTargetDimensions : function() {
             var anchor = this._anchor,
                 anchorOffset = anchor.offset();
