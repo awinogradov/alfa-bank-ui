79ae9bc01b66de69dd98d2faebe5caa41624a044
Daniel Sabelnikov <dsabelnikov@gmail.com>
2015-02-04 17:04:57 +0300

    removed message__control element

--- common.blocks/form-field/_has-validate/form-field_has-validate.browser.js
+++ common.blocks/form-field/_has-validate/form-field_has-validate.browser.js
@@ -14,8 +14,7 @@ FormField.decl({ block : this.name, modName : 'has-validate', modVal : true }, /
         this.__base.apply(this, arguments);
         if (this.hasMod('message')) {
             this.setMessageVal(this._status);
-            var message = this._getMessage();
-            message.setMod(message.elem('control'), 'error', true);
+            this._getMessage().setMod('error', true);
         }
     }
 });
