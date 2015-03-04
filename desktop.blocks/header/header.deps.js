349cf2d58c78e1d912cdcf2a6f8edf7635f4d62a
verybigman <winogradovaa@gmail.com>
2015-03-04 11:21:44 +0300

    header refactor

---
+++ desktop.blocks/header/header.deps.js
@@ -0,0 +1,16 @@
+[{
+    shouldDeps : [
+        { elems : ['inner', 'menu', 'user'] },
+        { block : 'logo', mods : { view : 'full' } },
+        { block : 'menu', mods : { horizontal : true } },
+        { block : 'menu-item', mods : { type : 'link' } },
+        { block : 'menu-item', mods : { type : 'dropdown' } }
+    ].concat([
+        { block : 'logo' },
+        { block : 'menu' },
+        { block : 'menu-item' }
+    ].map(function(block) {
+        block.mods = { theme : 'alfa', bkg : 'color' };
+        return block;
+    }))
+}]
