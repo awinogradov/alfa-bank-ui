1d2624878db9a3bd270c7040c1ebd3b098e5bada
Daniel Sabelnikov <dsabelnikov@gmail.com>
2015-05-12 19:06:10 +0300

    tests(form-field): form-field_has-validate test

---
+++ common.blocks/form-field/_has-validate/form-field_has-validate.deps.js
@@ -0,0 +1,7 @@
+[{
+    tech : 'spec.js',
+    shouldDeps : [
+        { tech : 'js', block : 'form-field', mods : { type : 'input', message : 'text' } },
+        { tech : 'bemhtml', block : 'form-field', mods : { type : 'input', message : 'text' } }
+    ]
+}]
