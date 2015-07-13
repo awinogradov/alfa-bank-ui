5bf3f0a2af9864270260c3e07643b4ea20389903
Ivan Voischev <voischev.ivan@ya.ru>
2015-07-13 11:38:11 +0300

    fix conflict

--- common.blocks/header/header.deps.js
+++ common.blocks/header/header.deps.js
@@ -8,17 +8,6 @@
         { block: 'logo', mods: { view: 'full' } },
         { block: 'menu', mods: { horizontal: true } },
         { block: 'menu-item', mods: { type: ['link', 'dropdown'] } },
-<<<<<<< HEAD
-    ].concat([
-        { block: 'logo' },
-        { block: 'menu' },
-        { block: 'menu-item' }
-    ].map(function(block) {
-        block.mods = { theme: 'alfa', bkg: 'color' };
-        return block;
-    }))
-=======
         { block: 'link', mods: { pseudo: true } }
     ]
->>>>>>> feature/NYCP-4-merged
 }];
