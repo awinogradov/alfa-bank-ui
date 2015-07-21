8c8c8042a1edeca36ac91357bbb37d1939e074f6
Ivan Voischev <voischev.ivan@ya.ru>
2015-07-21 18:03:21 +0300

    add examples blocks

---
+++ test.blocks/example-header/example-header.deps.js
@@ -0,0 +1,12 @@
+({
+    shouldDeps: [
+        {
+            block: 'heading',
+            mods: { theme: 'alfa-on-color' }
+        },
+        {
+            block: 'paragraph',
+            mods: { theme: 'alfa-on-color' }
+        }
+    ]
+})
