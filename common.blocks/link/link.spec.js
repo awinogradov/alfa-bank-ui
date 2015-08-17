17252dc974cbdc47d412b130596c65b5a12e22fb
Ivan Voischev <voischev.ivan@ya.ru>
2015-08-17 18:38:53 +0300

    feat(link, button): * add elem icon for mode icon, link add mode text
    BREAKING_CHANGE:·use mode text in link for text

---
+++ common.blocks/link/link.spec.js
@@ -0,0 +1,68 @@
+/* global modules:false */
+
+/**
+ * @spec link
+ */
+
+modules.define(
+    'spec',
+    ['link', 'i-bem__dom', 'jquery', 'BEMHTML', 'sinon', 'spec__utils'],
+    function(provide, Link, BEMDOM, $, BEMHTML, sinon, utils) {
+
+describe('link', function() {
+    var block,
+        bemjson;
+
+    beforeEach(function() {
+        bemjson = { block : 'link' };
+        block = utils.buildBlock('link', bemjson);
+    });
+
+    afterEach(function() {
+        utils.destruct(block);
+    });
+
+    describe('dom', function() {
+
+        it('content custom', function() {
+            bemjson.content = { cls: 'cls', content: 'custom' };
+            block = utils.buildBlock('link', bemjson);
+
+            block.domElem
+                .find('.cls')
+                .hasClass('cls')
+                .should.be.true;
+
+            block.domElem
+                .find('.link__text')
+                .hasClass('link__text')
+                .should.be.false;
+
+            block.domElem
+                .find('.link__icon')
+                .hasClass('link__icon')
+                .should.be.false;
+        });
+
+        it('mode text', function() {
+            bemjson.text = 'text'
+            block = utils.buildBlock('link', bemjson);
+
+            block.findElem('text')
+                .hasClass('link__text')
+                .should.be.true;
+        });
+
+        it('mode icon', function() {
+            bemjson.icon = { block: 'icon' };
+            block = utils.buildBlock('link', bemjson);
+            block.findElem('icon')
+                .hasClass('link__icon')
+                .should.be.true;
+        });
+    });
+});
+
+provide();
+
+});
