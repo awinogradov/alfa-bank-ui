3943160034d04908d0b1cd2793b434ebe3bf9d38
Ivan Voischev <voischev.ivan@ya.ru>
2015-08-21 15:28:44 +0300

    feat(footer): footer independent menu & cleanup

--- common.blocks/footer/footer.deps.js
+++ common.blocks/footer/footer.deps.js
@@ -1,9 +1,7 @@
 [{
     shouldDeps: [
-        { elems: ['inner', 'menu'] },
         { block: 'copyright' },
         { block: 'menu', mods: { horizontal: true } },
-        { block: 'menu' },
         { block: 'menu-item', type: 'link' }
     ]
 }];
