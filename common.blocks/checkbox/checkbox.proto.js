7cd0b5e4906e9ed222032ca6a052098742b4de81
Ivan Voischev <voischev.ivan@ya.ru>
2015-09-10 12:25:12 +0300

    feat(checkbox): checbox upd styles

---
+++ common.blocks/checkbox/checkbox.proto.js
@@ -0,0 +1,24 @@
+module.exports = {
+    themes: ['alfa-on-color', 'alfa-on-white'],
+    states: ['hovered', 'checked', ['hovered', 'checked'], 'focused', 'disabled', ['disabled', 'checked']],
+    size: ['m', 'l'],
+    type: ['button'],
+    options: {
+        default: {
+            name: 'default'
+        },
+        size: {
+            m: {
+                name: 'sizeM',
+            },
+            l: {
+                name: 'sizeL',
+            }
+        },
+        type: {
+            name: 'type'
+        }
+    },
+    default: { size: 'm' },
+    data: { text: 'Checkbox' }
+};
