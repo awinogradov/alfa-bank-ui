e32de97dc80d51f36d9f35d4c5f471570fad65c4
Ivan Voischev <voischev.ivan@ya.ru>
2015-11-11 11:47:43 +0300

    feat(social): add social block

---
+++ common.blocks/social/_theme/social_theme_alfa-on-color.deps.js
@@ -0,0 +1,12 @@
+({
+    shouldDeps: [
+        {
+            block: 'link',
+            mods: { theme: 'alfa-on-color' }
+        },
+        {
+            block: 'icon',
+            mods: { theme: 'alfa-on-color' }
+        }
+    ]
+});
