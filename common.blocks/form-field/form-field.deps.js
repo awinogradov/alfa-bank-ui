687fd7a7c793d9e8582526db686d706b0dae3c11
awinogradov <winogradovaa@gmail.com>
2016-02-25 11:48:54 +0300

    refactor(form): remove has-validation source

--- common.blocks/form-field/form-field.deps.js
+++ common.blocks/form-field/form-field.deps.js
@@ -6,8 +6,8 @@
     tech: 'spec.js',
     shouldDeps: [
         { tech: 'js', block: 'validation' },
-        { tech: 'js', block: 'form-field', mods: { type: 'input', 'has-validation': true, message: 'popup' } },
+        { tech: 'js', block: 'form-field', mods: { type: 'input', message: 'popup' } },
         { tech: 'bemhtml', block: 'input' },
-        { tech: 'bemhtml', block: 'form-field', mods: { type: 'input', 'has-validation': true, message: 'popup' } }
+        { tech: 'bemhtml', block: 'form-field', mods: { type: 'input', message: 'popup' } }
     ]
 }]);
